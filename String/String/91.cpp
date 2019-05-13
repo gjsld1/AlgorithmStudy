1f0d9a2f9cbd2296fe7e0086949aeb76c40f1184
신예지 <gjsld1@naver.com>
2019-05-13 19:52:14 +0900

    19/05/13 removeToOrder

---
+++ String/String/91.cpp
@@ -0,0 +1,9 @@
+//
+//  91.cpp
+//  String
+//
+//  Created by 신예지 on 04/05/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ String/String/removeToOrder.cpp
@@ -0,0 +1,50 @@
+//
+//  removeToOrder.cpp
+//  String
+//
+//  Created by 신예지 on 06/05/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int solution(string S) {
+    vector<pair<char,int>> alpha;
+    
+    for(int i=0; i<S.length(); i++) alpha.push_back(pair<char,int>(S[i], i));
+    sort(alpha.begin(), alpha.end());
+    
+    vector<int> sortOrder;
+    for(int i=0; i<alpha.size(); i++) sortOrder.push_back(alpha[i].second);
+    
+    vector<int>::iterator it=sortOrder.begin()+1;
+    while(it != sortOrder.end()-1) {
+        int before = *it-*(it-1);
+        int after = *(it+1)-*it;
+        if(before*after < 0) {
+            if(before<0) sortOrder.erase(it-1);
+            else sortOrder.erase(it+1);
+        }
+        else {
+            if(before<0) sortOrder.erase(it-1);
+            else {
+                if(abs(before)==abs(after)) it++;
+                else if(abs(before)>abs(after)) sortOrder.erase(it-1);
+                else sortOrder.erase(it+1);
+            }
+        }
+    }
+    
+    return alpha.size()-sortOrder.size();
+}
+
+int main() {
+    string str = "banana";
+    cout << solution(str);
+}

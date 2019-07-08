6048ef1fca8dc6c3a41ec034027cb2433c7bd829
신예지 <gjsld1@naver.com>
2019-07-08 16:21:22 +0900

    19/07/07 baekjoon 1654 cableCut WrongAnswer

--- BinarySearch/BinarySearch/cableCut.cpp
+++ BinarySearch/BinarySearch/cableCut.cpp
@@ -14,17 +14,13 @@ using namespace std;
 int main() {
     int k, n;
     cin >> k >> n;
-    
+
     long long* length = new long long[k];
-    long long sum = 0;
-    for(int i=0; i<k; i++) {
-        cin >> length[i];
-        sum += length[i];
-    }
+    for(int i=0; i<k; i++) cin >> length[i];
     sort(length, length+k);
     
     long long min = length[0]/n;
-    long long max = sum;
+    long long max = length[k-1];
     
     long long ans = 0;
     
@@ -34,13 +30,11 @@ int main() {
         
         if(mid != 0) for(int i=0; i<k; i++) count += length[i]/mid;
         
-        
         if(count>=n) {
             min = mid+1;
             ans = mid;
         }
         else max = mid-1;
     }
-    
     cout << ans;
 }

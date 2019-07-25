020208fc8992f0be11e44343b3f4293b1df2f2ca
신예지 <gjsld1@naver.com>
2019-07-25 17:39:21 +0900

    19/07/25 baekjoon 1918 postfixExpression

--- Stack/Stack/postfixExpression.cpp
+++ Stack/Stack/postfixExpression.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
 
 int priority(char c) {
@@ -20,28 +21,68 @@ int priority(char c) {
         case '*': case '/':
             return 2;
     }
-    return 0;
+    return -1;
 }
 
 int main() {
+    string input;
+    cin >> input;
+    
+    string ans = "";
+    stack<char> s;
+    
+    for(int i=0; i<input.length(); i++) {
+        if(input[i]>='A' && input[i]<='Z') ans+=input[i];
+        else if(input[i]=='(') s.push(input[i]);
+        else if(input[i]==')') {
+            while(s.top()!='(') {
+                ans+=s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        else {
+            if(s.empty()) s.push(input[i]);
+            else {
+                while(!s.empty() && priority(s.top())>=priority(input[i])) {
+                    ans+=s.top();
+                    s.pop();
+                }
+                s.push(input[i]);
+            }
+        }
+    }
+    while(!s.empty()) {
+        ans+=s.top();
+        s.pop();
+    }
+    
+    cout<< ans;
+}
+
+/*
+ * Memory limit
+ 
+int main() {
     char c;
     stack<char> operand;
+    string ans = "";
     
     while((c=cin.get())!='\n') {
-        if(c>=65 && c<=90) cout << c;
+        if(c>='A' && c<='Z') ans += c;
         else if(c=='(') operand.push(c);
         else if(c==')') {
             while(operand.top()!='(') {
-                cout << operand.top();
+                ans += operand.top();
                 operand.pop();
             }
             operand.pop();
         }
         else {
-            if(operand.empty() || priority(operand.top())<=priority(c)) operand.push(c);
+            if(operand.empty()) operand.push(c);
             else {
                 while(!operand.empty() && priority(operand.top()) >= priority(c)) {
-                    cout << operand.top();
+                    ans += operand.top();
                     operand.pop();
                 }
                 operand.push(c);
@@ -50,7 +91,10 @@ int main() {
     }
     
     while(!operand.empty()) {
-        cout << operand.top();
+        ans += operand.top();
         operand.pop();
     }
+    
+    cout << ans;
 }
+*/

2fc4694560c309717c153fc1e603f017b22b7346
신예지 <gjsld1@naver.com>
2019-07-29 16:24:29 +0900

    19/07/29 baekjoon 7453 sumZero TimeLimit using Map

--- BruteForce/BruteForce/sumZero.cpp
+++ BruteForce/BruteForce/sumZero.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
@@ -23,19 +24,23 @@ int main() {
     for(int i=0; i<n; i++) cin>>a[i]>>b[i]>>c[i]>>d[i];
     
     int* sumAB = new int[n*n];
-    int* sumCD = new int[n*n];
+    map<int,int> m;
+    
     int idx = 0;
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            sumAB[idx] = a[i]+b[j];
-            sumCD[idx++] = c[i]+d[j];
+            sumAB[idx++] = a[i]+b[j];
+            int temp = c[i]+d[j];
+            
+            if(m.find(temp)!=m.end()) m[temp]=m.find(temp)->second+1;//m.insert({temp,m.find(temp)->second+1});
+            else m.insert({temp,1});
         }
     }
-    sort(sumCD, sumCD+n*n);
     
     int ans = 0;
     for(int i=0; i<n*n; i++) {
-        if(binary_search(sumCD, sumCD+n*n, -sumAB[i])) ans++;
+        if(m.find(-sumAB[i])!=m.end()) ans+=m.find(-sumAB[i])->second;
     }
+    
     cout << ans;
 }

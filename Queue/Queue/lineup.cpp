f89cba33e1db064ae762d42f0a77aa7a55777a4c
신예지 <gjsld1@naver.com>
2019-07-30 23:42:15 +0900

    19/07/30 baekjoon 2252 lineup WrongAnswer

--- Queue/Queue/lineup.cpp
+++ Queue/Queue/lineup.cpp
@@ -1,4 +1,5 @@
 //
+//  baekjoon 2252
 //  lineup.cpp
 //  Queue
 //
@@ -7,83 +8,43 @@
 //
 
 #include <iostream>
+#include <queue>
+#include <utility>
+#define MAX 100001
 using namespace std;
-
-struct node {
-    int num;
-    struct node* pointer = NULL;
-};
 
 int main() {
     int n, m;
     cin >> n >> m;
     
-    int* make = new int[n+1]();
-    struct node* hash = new struct node[n+1];
+    int* priority = new int[n+1]();
     
     for(int i=0; i<m; i++) {
         int a, b;
         cin >> a >> b;
         
-        struct node *temp = new node;
-        temp->num = b;
-        temp->pointer = hash[a].pointer;
-        hash[a].pointer = temp;
-    }
-    
-    struct node* head = new node;
-    for(int i=1; i<=n; i++) {
-        struct node* next = hash[i].pointer;
-        if(next == NULL) continue;
-        struct node *temp = new node;
-        if(make[i]==0) {
-            temp->num = i;
-            make[i] = 1;
-            
-            if(head->pointer == NULL) head->pointer = temp;
-            else {
-                struct node* end = head->pointer;
-                while(end->pointer != NULL) {
-                    if(end->pointer->num == next->num) break;
-                    end = end->pointer;
-                }
-                temp->pointer = end->pointer;
-                end->pointer = temp;
-                
-            }
+        if(priority[a]==0 && priority[b]==0) {
+            priority[a]=MAX;
+            priority[b]=MAX-1;
         }
-        
-        struct node* pos = head->pointer;
-        while(pos->num != i && pos->pointer != NULL) {
-            if(pos->pointer->num == next->num) {
-                temp->pointer = pos->pointer;
-                pos->pointer = temp;
-                next = next->pointer;
-                break;
-            }
-            pos = pos->pointer;
+        else if(priority[a]==0 && priority[b]!=0) {
+            priority[a] = priority[b]+1;
         }
-        while(next != NULL) {
-            if(make[next->num]==1) {
-                next = next->pointer;
-                break;
-            }
-            struct  node* after = new node;
-            after->num = next->num;
-            make[next->num] = 1;
-            after->pointer = pos->pointer;
-            
-            pos->pointer = after;
-            next = next->pointer;
+        else if(priority[a]!=0 && priority[b]==0) {
+            priority[b] = priority[a]-1;
+        }
+        else {
+            priority[a] = priority[b]+1;
         }
     }
     
-    struct node* print = head->pointer;
-    while(print != NULL) {
-        cout << print->num;
-        print = print->pointer;
-    }
+    priority_queue<pair<int, int>> pq;
     for(int i=1; i<=n; i++) {
-        if(make[i]==0) cout << i;
+        pq.push({priority[i], i});
+    }
+    
+    for(int i=0; i<n; i++) {
+        cout << pq.top().second;
+        pq.pop();
     }
 }

a25f1276328e57f0274170c13feb72d841542092
신예지 <gjsld1@naver.com>
2019-07-31 16:33:44 +0900

    19/07/31 baekjoon 2252 lineup TimeLimit using queue&priority_queue

--- Queue/Queue/lineup.cpp
+++ Queue/Queue/lineup.cpp
@@ -7,6 +7,7 @@
 //  Copyright © 2019 신예지. All rights reserved.
 //
 
+/*
 #include <iostream>
 #include <queue>
 #include <utility>
@@ -44,6 +45,67 @@ int main() {
     }
     
     for(int i=0; i<n; i++) {
+        cout << pq.top().second;
+        pq.pop();
+    }
+}
+*/
+
+#include <iostream>
+#include <queue>
+#include <utility>
+#define MAX 10
+using namespace std;
+
+int main() {
+    struct node {
+        int n;
+        node* pointer = NULL;
+    };
+    
+    int n, m;
+    cin >> n >> m;
+    
+    int* priority = new int[n+1]();
+    node* hash = new node[n+1];
+    
+    for(int i=0; i<m; i++) {
+        int a, b;
+        cin >> a >> b;
+        
+        node* temp = new node;
+        temp->n = b;
+        temp->pointer = hash[a].pointer;
+        
+        hash[a].pointer = temp;
+    }
+    
+    queue<int> q;
+    for(int i=1; i<=n; i++) {
+        if(priority[i]==0) priority[i]=MAX;
+        q.push(i);
+        
+        while(!q.empty()) {
+            node* temp = hash[q.front()].pointer;
+            while(temp!=NULL) {
+                q.push(temp->n);
+                
+                if(priority[temp->n]==0) priority[temp->n] = priority[q.front()]-1;
+                else priority[temp->n] -= 1;
+                
+                temp = temp->pointer;
+            }
+            q.pop();
+        }
+    }
+    for(int i=1; i<=n; i++) cout << priority[i] << endl;
+    
+    priority_queue<pair<int,int>> pq;
+    for(int i=1; i<=n; i++) {
+        pq.push({priority[i],i});
+    }
+    
+    for(int i=1; i<=n; i++) {
         cout << pq.top().second;
         pq.pop();
     }

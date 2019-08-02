664c625ca8d93fda0570e8081e886ff3affcc6d8
신예지 <gjsld1@naver.com>
2019-08-02 23:51:39 +0900

    19/08/02 baekjoon 2252 lineup WrongAnswer using topology

--- Queue/Queue/lineup.cpp
+++ Queue/Queue/lineup.cpp
@@ -9,61 +9,36 @@
 
 #include <iostream>
 #include <queue>
-#include <utility>
-#define MAX 10
+#include <vector>
 using namespace std;
 
 int main() {
-    struct node {
-        int n;
-        node* pointer = NULL;
-    };
-    
     int n, m;
     cin >> n >> m;
     
-    int* priority = new int[n+1]();
-    node* hash = new node[n+1];
+    int* inDegree = new int[n+1]();
+    vector<int> connet[n+1];
     
     for(int i=0; i<m; i++) {
         int a, b;
         cin >> a >> b;
         
-        node* temp = new node;
-        temp->n = b;
-        temp->pointer = hash[a].pointer;
-        
-        hash[a].pointer = temp;
+        connet[a].push_back(b);
+        inDegree[b]++;
     }
     
     queue<int> q;
     for(int i=1; i<=n; i++) {
-        if(priority[i]==0) priority[i]=MAX;
-        else continue;
-        q.push(i);
+        if(inDegree[i]==0) q.push(i);
+    }
+    
+    for(int i=1; i<=n; i++) {
+        int front = q.front();
+        q.pop();
         
-        while(!q.empty()) {
-            node* temp = hash[q.front()].pointer;
-            while(temp!=NULL) {
-                q.push(temp->n);
-                
-                if(priority[temp->n]==0) priority[temp->n] = priority[q.front()]-1;
-                else priority[temp->n] -= 1;
-                
-                temp = temp->pointer;
-            }
-            q.pop();
+        for(int i=0; i<connet[front].size(); i++) {
+            if(--inDegree[connet[front][i]]==0) q.push(connet[front][i]);
         }
-    }
-    for(int i=1; i<=n; i++) cout << priority[i] << endl;
-    
-    priority_queue<pair<int,int>> pq;
-    for(int i=1; i<=n; i++) {
-        pq.push({priority[i],i});
-    }
-    
-    for(int i=1; i<=n; i++) {
-        cout << pq.top().second;
-        pq.pop();
+        cout << front;
     }
 }

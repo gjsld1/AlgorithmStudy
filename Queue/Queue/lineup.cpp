d63fffb5ea13b92db9e7635f88fadbac2c297d38
신예지 <gjsld1@naver.com>
2019-08-02 22:43:29 +0900

    19/08/02 baekjoon 2252 lineup TimeLimit

--- Queue/Queue/lineup.cpp
+++ Queue/Queue/lineup.cpp
@@ -7,50 +7,6 @@
 //  Copyright © 2019 신예지. All rights reserved.
 //
 
-/*
-#include <iostream>
-#include <queue>
-#include <utility>
-#define MAX 100001
-using namespace std;
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-    
-    int* priority = new int[n+1]();
-    
-    for(int i=0; i<m; i++) {
-        int a, b;
-        cin >> a >> b;
-        
-        if(priority[a]==0 && priority[b]==0) {
-            priority[a]=MAX;
-            priority[b]=MAX-1;
-        }
-        else if(priority[a]==0 && priority[b]!=0) {
-            priority[a] = priority[b]+1;
-        }
-        else if(priority[a]!=0 && priority[b]==0) {
-            priority[b] = priority[a]-1;
-        }
-        else {
-            priority[a] = priority[b]+1;
-        }
-    }
-    
-    priority_queue<pair<int, int>> pq;
-    for(int i=1; i<=n; i++) {
-        pq.push({priority[i], i});
-    }
-    
-    for(int i=0; i<n; i++) {
-        cout << pq.top().second;
-        pq.pop();
-    }
-}
-*/
-
 #include <iostream>
 #include <queue>
 #include <utility>
@@ -83,6 +39,7 @@ int main() {
     queue<int> q;
     for(int i=1; i<=n; i++) {
         if(priority[i]==0) priority[i]=MAX;
+        else continue;
         q.push(i);
         
         while(!q.empty()) {

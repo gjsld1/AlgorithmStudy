c6dddc23481bebd700aa83b3d332200fdb1f6e0a
신예지 <gjsld1@naver.com>
2019-08-17 03:06:37 +0900

    19/08/17 baekjoon 14502 lab WrongAnswer using bfs

--- BFS/BFS/lab.cpp
+++ BFS/BFS/lab.cpp
@@ -9,10 +9,11 @@
 
 #include <iostream>
 #include <utility>
+#include <queue>
 #include <vector>
 using namespace std;
 
-#define empty 0
+#define safe 0
 #define wall 1
 #define virus 2
 
@@ -24,10 +25,12 @@ int main() {
     for(int i=0; i<n; i++) lab[i] = new int[m];
     
     vector<pair<int,int>> safeZone;
+    queue<pair<int,int>> q;
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
             cin >> lab[i][j];
-            if(lab[i][j]==empty) safeZone.push_back({i,j});
+            if(lab[i][j]==safe) safeZone.push_back({i,j});
+            else if(lab[i][j]==virus) q.push({i,j});
         }
     }
     
@@ -38,7 +41,6 @@ int main() {
             for(int k=j+1; k<size; k++) {
                 int** temp = new int*[n];
                 for(int l=0; l<n; l++) temp[l]=new int[m];
-                
                 for(int p=0; p<n; p++) {
                     for(int q=0; q<m; q++) {
                         temp[p][q]=lab[p][q];
@@ -48,28 +50,38 @@ int main() {
                 temp[safeZone[j].first][safeZone[j].second] = 1;
                 temp[safeZone[k].first][safeZone[k].second] = 1;
                 
-                for(int p=0; p<n; p++) {
-                    for(int q=0; q<m; q++) {
-                        if(temp[p][q]==2) {
-                            int tempX=p, tempY=q;
-                            while(--tempX>=0 && temp[tempX][tempY]!=wall) temp[tempX][tempY]=virus;
-                            tempX=p;
-                            while(++tempX<n && temp[tempX][tempY]!=wall) temp[tempX][tempY]=virus;
-                            tempX=p;
-                            while(--tempY>=0 && temp[tempX][tempY]!=wall) temp[tempX][tempY]=virus;
-                            tempY=q;
-                            while(++tempY<m && temp[tempX][tempY]!=wall) temp[tempX][tempY]=virus;
-                        }
+                
+                while(!q.empty()) {
+                    pair<int,int> front = q.front();
+                    int x=front.first;
+                    int y=front.second;
+                    q.pop();
+                    
+                    if(x-1>=0 && temp[x-1][y]==safe) {
+                        temp[x-1][y]=2;
+                        q.push({x-1,y});
+                    }
+                    if(x+1<n && temp[x+1][y]==safe) {
+                        temp[x+1][y]=2;
+                        q.push({x+1,y});
+                    }
+                    if(y-1>=0 && temp[x][y-1]==safe) {
+                        temp[x][y-1]=2;
+                        q.push({x,y-1});
+                    }
+                    if(y+1<m && temp[x][y+1]==safe) {
+                        temp[x][y+1]=2;
+                        q.push({x,y+1});
                     }
                 }
                 
-                int safe = 0;
+                int tmp = 0;
                 for(int p=0; p<n; p++) {
                     for(int q=0; q<m; q++) {
-                        if(temp[p][q]==0) safe++;
+                        if(temp[p][q]==0) tmp++;
                     }
                 }
-                if(ans<safe) ans=safe;
+                if(ans<tmp) ans=tmp;
             }
         }
     }

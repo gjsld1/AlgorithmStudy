6e6c27c6ba010f91a0c5f509cf72d73619ce844f
신예지 <gjsld1@naver.com>
2019-08-19 01:45:14 +0900

    19/08/19 baekjoon 1987 alphaget

--- DFS/DFS/alphabet.cpp
+++ DFS/DFS/alphabet.cpp
@@ -8,6 +8,62 @@
 //
 
 #include <iostream>
+using namespace std;
+
+int r, c;
+char** map;
+int** route;
+
+bool isInRange(int x, int y) {
+    if(x>=0 && x<r && y>=0 && y<c) return true;
+    return false;
+}
+
+void bfs(int* visited, int ans, int x, int y) {
+    int temp[26];
+     for(int i=0; i<26; i++) temp[i]=visited[i];
+    
+    temp[map[x][y]-'A']=1;
+    ans++;
+    route[x][y] = ans<route[x][y]?route[x][y]:ans;
+    
+    if(isInRange(x+1,y) && temp[map[x+1][y]-'A']==0) bfs(temp,ans,x+1,y);
+    if(isInRange(x-1,y) && temp[map[x-1][y]-'A']==0) bfs(temp,ans,x-1,y);
+    if(isInRange(x,y+1) && temp[map[x][y+1]-'A']==0) bfs(temp,ans,x,y+1);
+    if(isInRange(x,y-1) && temp[map[x][y-1]-'A']==0) bfs(temp,ans,x,y-1);
+}
+
+int main() {
+    cin >> r >> c;
+    
+    map = new char* [r];
+    route = new int* [r];
+    for(int i=0; i<r; i++) {
+        map[i] = new char[c];
+        route[i] = new int[c]();
+        for(int j=0; j<c; j++) {
+            cin >> map[i][j];
+        }
+    }
+    
+    int visited[26] = {0};
+    int ans=0;
+    bfs(visited, ans, 0, 0);
+    
+    int max=0;
+    for(int i=0; i<r; i++) {
+        for(int j=0; j<c; j++) {
+            max = max<route[i][j] ? route[i][j] : max;
+        }
+    }
+    cout << max;
+}
+
+/*
+ * using unnecessary stack
+ *
+ 
+#include <iostream>
 #include <stack>
 #include <utility>
 using namespace std;
@@ -24,7 +80,7 @@ bool isInRange(int x, int y) {
 
 void bfs(int* visited, int ans) {
     int temp[26];
-     for(int i=0; i<26; i++) temp[i]=visited[i];
+    for(int i=0; i<26; i++) temp[i]=visited[i];
     
     while(!s.empty()) {
         int x = s.top().first;
@@ -81,3 +137,4 @@ int main() {
     }
     cout << max;
 }
+*/

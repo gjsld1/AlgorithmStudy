3efb052e80958196da2dba1670b54b524e7fad60
신예지 <gjsld1@naver.com>
2019-08-22 02:05:58 +0900

    19/08/22 kakao 2019-1st candidateKey 82.1%

--- BFS/BFS/candidateKey.cpp
+++ BFS/BFS/candidateKey.cpp
@@ -6,7 +6,6 @@
 //  Created by 신예지 on 19/08/2019.
 //  Copyright © 2019 신예지. All rights reserved.
 //
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -40,7 +39,6 @@ int solution(vector<vector<string>> relation) {
             
             for(int j=0; j<front.size(); j++) {
                 temp += relation[i][front[j]-'0'];
-                temp += " ";
             }
             s.insert(temp);
         }
@@ -71,3 +69,45 @@ int main() {
     
     cout << solution(v);
 }
+
+/* using vector 42.9%
+ 
+int solution(vector<vector<string>> relation) {
+    int answer=0;
+    
+    int colNum = relation[0].size();
+    int rowNum = relation.size();
+    
+    queue<vector<int>> q;
+    vector<vector<int>> candidate;
+    for(int i=0; i<colNum; i++) q.push({i});
+    ㅍ
+    while(!q.empty()) {
+        vector<int> front=q.front();
+        q.pop();
+        
+        set<vector<string>> s;
+        for(int i=0; i<rowNum; i++) {
+            vector<string> temp;
+            
+            for(int j=0; j<front.size(); j++) {
+                temp.push_back(relation[i][front[j]]);
+            }
+            s.insert(temp);
+        }
+        
+        if(s.size()==rowNum) {
+            candidate.push_back(front);
+        }
+        else {
+            for(int i=front[front.size()-1]+1; i<colNum; i++) {
+                vector<int> tmp = front;
+                tmp.push_back(i);
+                q.push(tmp);
+            }
+        }
+    }
+    answer = candidate.size();
+    return answer;
+}
+*/

1eb2073060404f0028f79ed5aa6dae0ebcf52c78
신예지 <gjsld1@naver.com>
2019-09-04 21:01:05 +0900

    19/09/04 baekjoon 4963 numOfIsland using dfs

--- Graph/Graph/numOfIsland.cpp
+++ Graph/Graph/numOfIsland.cpp
@@ -22,18 +22,32 @@ bool isInRange(int x, int y, int w, int h){
     return false;
 }
 
+void dfs(int** visited, int x, int y, int group) {
+    if(visited[x][y]==1) return;
+    visited[x][y]=1;
+    
+    if(isInRange(x,y-1,w,h) && visited[x][y-1]==0) dfs(visited, x, y-1, 1);
+    if(isInRange(x,y+1,w,h) && visited[x][y+1]==0) dfs(visited, x, y+1, 1);
+    if(isInRange(x-1,y,w,h) && visited[x-1][y]==0) dfs(visited, x-1, y, 1);
+    if(isInRange(x-1,y-1,w,h) && visited[x-1][y-1]==0) dfs(visited, x-1, y-1, 1);
+    if(isInRange(x-1,y+1,w,h) && visited[x-1][y+1]==0) dfs(visited, x-1, y+1, 1);
+    if(isInRange(x+1,y,w,h) && visited[x+1][y]==0) dfs(visited, x+1, y, 1);
+    if(isInRange(x+1,y-1,w,h) && visited[x+1][y-1]==0) dfs(visited, x+1, y-1, 1);
+    if(isInRange(x+1,y+1,w,h) && visited[x+1][y+1]==0) dfs(visited, x+1, y+1, 1);
+}
+
 int main() {
     while(1) {
         cin >> w >> h;
         if(w==0 || h==0) break;
         
-        int** board = new int*[h];
-        int** visited = new int*[h];
+        int** board = new int*[w];
+        int** visited = new int*[w];
         queue<pair<int,int>> q;
         
         for(int i=0; i<h; i++) {
-            board[i] = new int[w];
-            visited[i] = new int[w]();
+            board[i] = new int[h];
+            visited[i] = new int[h]();
             
             for(int j=0; j<h; j++) {
                 cin >> board[i][j];
@@ -42,31 +56,15 @@ int main() {
         }
         
         int ans=0;
-        while(!q.empty()){
+        while(!q.empty()) {
             int x = q.front().first;
             int y = q.front().second;
             q.pop();
             
             if(visited[x][y]==1) continue;
+            dfs(visited, x, y, 0);
             ans++;
         }
         cout << ans << endl;
     }
-}
-
-int dfs(int** visited, queue<pair<int,int>> q, int ans, int x, int y) {
-    if(visited[x][y]==1) return ans;
-    visited[x][y]=1;
-    
-    if(isInRange(x,y-1,w,h) && visited[x][y-1]==0) dfs(visited, q, ans, x, y-1);
-    if(isInRange(x,y+1,w,h) && visited[x][y+1]==0) visited[x][y+1]=1;
-    if(isInRange(x-1,y,w,h) && visited[x-1][y]==0) visited[x-1][y]=1;
-    if(isInRange(x-1,y-1,w,h) && visited[x-1][y-1]==0) visited[x-1][y-1]=1;
-    if(isInRange(x-1,y+1,w,h) && visited[x-1][y+1]==0) visited[x-1][y+1]=1;
-    if(isInRange(x+1,y,w,h) && visited[x+1][y]==0) visited[x+1][y]=1;
-    if(isInRange(x+1,y-1,w,h) && visited[x+1][y-1]==0) visited[x+1][y-1]=1;
-    if(isInRange(x+1,y+1,w,h) && visited[x+1][y+1]==0) visited[x+1][y+1]=1;
-    ans++;
-    
-    return ans;
 }

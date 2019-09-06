4c6dc89ac04a4dd68a9d77e2c8c5ab6e575d857e
신예지 <gjsld1@naver.com>
2019-09-07 00:42:42 +0900

    19/09/07 programmers noncompleteAthlete TimeLimit

--- Hash/Hash/noncompleteAthlete.cpp
+++ Hash/Hash/noncompleteAthlete.cpp
@@ -18,11 +18,13 @@ string solution(vector<string> participant, vector<string> completion) {
     
     sort(participant.begin(), participant.end());
     sort(completion.begin(), completion.end());
+    
     for(int i=0; i<completion.size(); i++) {
         if(binary_search(participant.begin(), participant.end(), completion[i])) {
             participant.erase(find(participant.begin(), participant.end(), completion[i]));
         }
     }
+    
     answer = participant[0];
     return answer;
 }
@@ -31,6 +33,7 @@ int main() {
     vector<string> participant;
     vector<string> completion;
     
+    //ex3
     participant.push_back("marina");
     participant.push_back("josipa");
     participant.push_back("nikola");

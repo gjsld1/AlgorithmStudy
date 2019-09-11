f6d23e118535502b5833e7f80ccb69acb296fa64
신예지 <gjsld1@naver.com>
2019-09-11 17:05:34 +0900

    19/09/11 SWExpert 8338 calculator TimeLimit

--- SWexpert/SWexpert/calculator.cpp
+++ SWexpert/SWexpert/calculator.cpp
@@ -22,7 +22,7 @@ int main() {
         int sum=0;
         for(int j=0; j<n; j++) {
             cin >> num[j];
-            if(sum==0 || num[j]==0) sum+=num[j];
+            if(sum*num[j]==0) sum+=num[j];
             else if(sum==1 || num[j]==1) sum+=num[j];
             else sum*=num[j];
         }

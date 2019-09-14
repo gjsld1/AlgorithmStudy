e020523fafbd9d7e3aeadbda6f4c726b52433e3f
신예지 <gjsld1@naver.com>
2019-09-14 22:00:56 +0900

    19/09/14 SWExpert 8338 calculator

--- SWexpert/SWexpert/calculator.cpp
+++ SWexpert/SWexpert/calculator.cpp
@@ -27,11 +27,9 @@ int main() {
         for(int j=1; j<n; j++) {
             scanf("%d", &num[j]);
             
-            if(sum==0 || num[j]==0) continue;
-            else if(sum==1 || num[j]==1) sum+=num[j];
-            else sum*=num[j];
+            sum = (sum*num[j])>(sum+num[j]) ?  sum*num[j] : sum+num[j];
         }
         
-        printf("#%d %d",i+1,sum);
+        printf("#%d %d\n",i+1,sum);
     }
 }

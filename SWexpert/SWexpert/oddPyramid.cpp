bc4639c0bce8c79043b11ce48c314f6021dd200f
신예지 <gjsld1@naver.com>
2019-09-17 22:57:10 +0900

    19/09/17 SWExpertAcademy 8016 oddPyramid WrongAnswer

---
+++ SWexpert/SWexpert/oddPyramid.cpp
@@ -0,0 +1,30 @@
+//
+//  SWExpertAcademy 8016
+//  oddPyramid.cpp
+//  SWexpert
+//
+//  Created by 신예지 on 15/09/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    
+    for(int i=0; i<tc; i++) {
+        int n;
+        cin >> n;
+        
+        int lineOddCount = 2*n-1;
+        int beforeOddCount = pow((lineOddCount-1)/2,2);
+        
+        int lineStart = 2*beforeOddCount+1;
+        int lineEnd = lineStart+(2*(lineOddCount-1));
+        
+        cout << "#" << i+1 << " " << lineStart << " " << lineEnd << endl;
+    }
+}

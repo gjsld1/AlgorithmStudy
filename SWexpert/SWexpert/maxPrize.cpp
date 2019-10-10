d35b8b6265af8086d65d2774ffecef7cccbc9ce4
신예지 <gjsld1@naver.com>
2019-10-10 20:41:32 +0900

    19/10/10 swea 1244 maxPrizeeeeee

---
+++ SWexpert/SWexpert/maxPrize.cpp
@@ -0,0 +1,47 @@
+//
+//  SWExpertAcademy 1244 D3
+//  maxPrize.cpp
+//  SWexpert
+//
+//  Created by 신예지 on 10/10/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string score;
+int n, ans;
+
+void dfs(int cur, int num) {
+    if(num==n) {
+        ans = max(ans, atoi(score.c_str()));
+        return;
+    }
+    else {
+        for(int i=cur; i<score.size(); i++) {
+            for(int j=i+1; j<score.size(); j++) {
+                if(score[i]<=score[j]) {
+                    swap(score[i],score[j]);
+                    dfs(i, num+1);
+                    swap(score[i],score[j]);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int tc;
+    cin >> tc;
+    
+    for(int i=1; i<=tc; i++){
+        cin >> score >> n;
+        ans=0;
+        dfs(0,0);
+        
+        cout << "#" << i << " " << ans << endl;
+    }
+}

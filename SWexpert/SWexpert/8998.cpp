33a0991da0c2d7332b0451bba040f2f2cb4bb552
신예지 <gjsld1@naver.com>
2020-03-24 16:10:00 +0900

    20/03/24 swea 8998 wrong answer

--- SWexpert/SWexpert/8998.cpp
+++ SWexpert/SWexpert/8998.cpp
@@ -5,7 +5,7 @@
 //  Created by 신예지 on 23/03/2020.
 //  Copyright © 2020 신예지. All rights reserved.
 //
-
+/*
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -32,6 +32,7 @@ int main() {
         for(int i=0; i<n; i++) {
             int check=0;
             int lastDay=v[i].first;
+            
             while(check != v[i].second) {
                 if(!schedule[lastDay]) {
                     schedule[lastDay]=1;
@@ -41,9 +42,51 @@ int main() {
             }
         }
         
-        int idx=1;
-        while(!schedule[idx]) idx++;
+        int startDay=1;
+        while(!schedule[startDay]) startDay++;
         
-        cout << "#" << c << " " << idx-1 << endl;
+        cout << "#" << c << " " << startDay-1 << endl;
+    }
+}
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL); // 시간초과 방지
+    
+    int tc, c=1;
+    cin >> tc;
+    
+    while(tc--) {
+        int n;
+        cin >> n;
+        
+        vector<pair<int,int>> v;
+        for(int i=0; i<n; i++) {
+            int t, d;
+            cin >> t >> d;
+            v.push_back({d,t});
+        }
+        sort(v.begin(),v.end());
+        
+        int endDay= v[n-1].first;
+        int startDay = endDay-v[n-1].second+1;
+        
+        for(int i=n-2; i>=0; i--) {
+            if(v[i].first>=startDay) {
+                int originStart = startDay;
+                startDay = v[i].first-v[i].second+1;
+                startDay -= v[i].first-originStart+1;
+            }
+            else startDay = v[i].first-v[i].second+1;
+        }
+        
+        cout << "#" << c << " " << startDay-1 << endl;
     }
 }

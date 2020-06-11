18b0043c5f74a5e3ca61518334cc5c7123bc0fe0
신예지 <gjsld1@naver.com>
2020-06-11 15:21:51 +0900

    20/06/11 programmers developFunction

--- programmers/programmers/developFunction.cpp
+++ programmers/programmers/developFunction.cpp
@@ -25,20 +25,35 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     int start=v[0];
     int submit=1;
     vector<int> answer;
-    for(int i=1; i<v.size(); i++) {
-        if(start>v[i]) submit++;
+    for(int i=1; i<=v.size(); i++) {
+        if(i==v.size()) {
+            answer.push_back(submit);
+            break;
+        }
+        if(start>=v[i]) submit++;
         else {
             answer.push_back(submit);
             start=v[i];
             submit=1;
         }
     }
-    answer.push_back(submit);
     return answer;
 }
 
 int main() {
     vector<int> progresses, speeds;
+    
+    progresses.push_back(99);
+    progresses.push_back(99);
+    progresses.push_back(99);
+    progresses.push_back(99);
+    progresses.push_back(99);
+    speeds.push_back(3);
+    speeds.push_back(3);
+    speeds.push_back(3);
+    speeds.push_back(3);
+    speeds.push_back(3);
+    /*
     progresses.push_back(93);
     progresses.push_back(30);
     progresses.push_back(55);
@@ -46,7 +61,10 @@ int main() {
     speeds.push_back(1);
     speeds.push_back(30);
     speeds.push_back(5);
+     */
     
     vector<int> temp=solution(progresses,speeds);
-    cout << temp[0] <<" " << temp[1] << endl;
+    for (int i=0; i<temp.size(); i++) {
+        cout << temp[i] << " ";
+    }
 }
